86ac49706e290f471183d184a151a89b2121f30e
samithiwat <samithiwat2544@gmail.com>
2021-03-29 19:33:36 +0700

    Added skip space bar and escape exit with new input and update package

---
+++ src/input/Input_StartMenu.java
@@ -0,0 +1,22 @@
+package input;
+
+import update.CloseGame;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+
+public class Input_StartMenu{
+	public static boolean isSkip = false;
+	
+	public static void input(Scene scene) {
+		scene.setOnKeyPressed(key -> {
+			if(key.getCode() == KeyCode.SPACE) {
+				isSkip = true;
+			}
+			else if(key.getCode() == KeyCode.ESCAPE) {
+				CloseGame.setIsCloseGame(true);
+			}
+		});
+	}
+	
+	
+}
