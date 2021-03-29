c2e79ba17e38eedccaf0845c6e714d5e5fb1e4d1
samithiwat <samithiwat2544@gmail.com>
2021-03-29 19:23:36 +0700

    Edit Class MenuButton
    - now  you must enter color as parameter

--- src/gui/enity/MenuButton.java
+++ src/gui/enity/MenuButton.java
@@ -13,12 +13,13 @@ import logic.AudioLoader;
 
 public class MenuButton extends Button implements Clickable {
 	
-	private final String FONT_PATH_REGULAR = "font/BaiJamjuree-Regular.ttf";
-	private final String FONT_PATH_BOLD = "font/BaiJamjuree-Bold.ttf";
+	private final String FONT_PATH_REGULAR = "font/Bai_Jamjuree/BaiJamjuree-Regular.ttf";
+	private final String FONT_PATH_BOLD = "font/Bai_Jamjuree/BaiJamjuree-Bold.ttf";
 	private final String FONT_PATH = "font/Bai_Jamjuree/BaiJamjuree-Bold.ttf";
 
-	public MenuButton(String content,int contentSize,int width,int height) {
-		getStyleClass().add("mainmenu-button");
+	public MenuButton(String content,int contentSize,int width,int height,Color textColor) {
+		setTextFill(textColor);
+		getStyleClass().add("mainmenu-button-release-style");
 		setText(content);
 		setFontRegular(contentSize);
 		setPrefHeight(height);
@@ -28,20 +29,28 @@ public class MenuButton extends Button implements Clickable {
 
 	@Override
 	public void interact() {
+		setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				getStyleClass().add("mainmenu-button-release-style");
+			}
+			
+		});
 		setOnMouseEntered(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
 				AudioClip effect = AudioLoader.mouseEnterSound;
 				effect.play();
-				
+				getStyleClass().add("mainmenu-button-hold-style");
 			}
 			
 		});
 	}
 
 	public void setFontRegular(int size) {
-		System.out.println(getClass().getClassLoader().getResource(FONT_PATH_REGULAR));
+		//System.out.println(getClass().getClassLoader().getResource(FONT_PATH_REGULAR));
 		setFont(Font.font("Bai Jamjuree",FontWeight.NORMAL,size));
 	}
 	
