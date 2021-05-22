11e21dab1a0a27271277ca51b5adaf2f8162f172
samithiwat <samithiwat2544@gmail.com>
2021-05-23 00:02:33 +0700

    Clean Code

--- src/component/Constant.java
+++ src/component/Constant.java
@@ -16,7 +16,6 @@ public interface Constant {
 
 	String FONT_PATH_REGULAR = "font/Bai_Jamjuree/BaiJamjuree-Regular.ttf";
 	String FONT_PATH_BOLD = "font/Bai_Jamjuree/BaiJamjuree-Bold.ttf";
-	String FONT_PATH = "font/Bai_Jamjuree/BaiJamjuree-Bold.ttf";
 	
 	ImageCursor MOUSE_NORMAL = new ImageCursor((new Image(ClassLoader.getSystemResource("img/icon/MouseCursor.png").toString())));
 	ImageCursor MOUSE_SELECT = new ImageCursor((new Image(ClassLoader.getSystemResource("img/icon/MouseCursorSelected.png").toString())));
